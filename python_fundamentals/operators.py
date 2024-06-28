feead65e8137ffa90a64a6251b9b8be50f33e072
sourabh <Sourabh7iwari@gmail.com>
2024-06-28 14:07:21 +0530

    made two py files, datatype and operators and defined these topics

---
+++ python_fundamentals/operators.py
@@ -0,0 +1,26 @@
+"""Arithmetic operation in python are ( + , - , // , / , % , ** )"""
+print(2+3.0)
+#output -> 5.0
+
+print(3-1)
+#output -> 2
+
+""" the operator '%' return the remainder of division"""
+print(10%3,10%7,20%2)
+#output -> 1 0 0
+
+print(24//3, 24//4)
+#output(the only integer part of divisor if not perfect divisor ) -> 8 6 
+
+print(100/3,24//4)
+#output(the whole divisor) -> 33.3333... 6
+
+print(4**2)
+#output -> 16
+
+"""Bitwise operation in python are  ( & , | , < < , > > , ~ , ^ )"""
+print(2 & 3, 2 & 4)
+#output -> 2
+
+print(2 | 3, 2 | 4)
+#output -> 3
