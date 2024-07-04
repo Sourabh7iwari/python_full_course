90f02f557529781aa5f965cce79dea8e85c27438
Sourabh Tiwari <Sourabh7iwari@gmail.com>
2024-07-04 16:02:19 +0000

    completed py fundamental notebook

--- python_fundamentals/datatype.py
+++
@@ -1,8 +0,0 @@
-"""Python can output anything by function 'print()' but string need to be under quotes """
-print("hello world")
-#output -> hellow world
-
-#predefined data type in python are int, float, str
-print(4,"python is great,",3.16)
-#output -> 4 "python is great ",3.16
-
